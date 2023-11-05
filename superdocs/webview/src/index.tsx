2436400ab93722cbbc27722acad09ee22e052f65
Vijay Daita <hello@daita.com>
2023-11-04 22:58:42 -0500

    Codebase embedding

--- superdocs/webview/src/index.tsx
+++ superdocs/webview/src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider defaultColorScheme="dark">
       <App />
     </MantineProvider>
   // </React.StrictMode>
